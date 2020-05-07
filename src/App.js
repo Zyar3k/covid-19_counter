9c393f12f9a00130cc77143e15f119a3fcff6d4b
Arkadiusz Zygan <a.zygan@gmail.com>
2020-05-07 21:19:01 +0200

    Add first template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Cards from './components/Cards/Cards';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import Chart from './components/Chart/Chart';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>covid019</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 
 export default App;
