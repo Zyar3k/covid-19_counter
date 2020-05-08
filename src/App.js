e591e84c7a20b278b3cb441544da3932af2441c1
Arkadiusz Zygan <a.zygan@gmail.com>
2020-05-08 14:22:55 +0200

    Connect to api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import Cards from './components/Cards/Cards';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import Chart from './components/Chart/Chart';
+import { fetchData } from './api';
 
 class App extends Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
